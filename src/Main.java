a61e28531fc2fa529396c7a3a8c02faf4be55eda
FeyCode4779 <feysalbubshe@gmail.com>
2024-02-28 14:38:30 +0300

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        //welocme to advanced java
+        //welocme to advanced  ---------
         System.out.println("Hello world!");
+
+        int number1 =1 ;
+        int number2 = 3;
+
+        int average = (number1+number2) / 2;
+        System.out.println("Average: " + average);
     }
 }
